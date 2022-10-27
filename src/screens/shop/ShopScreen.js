4c8cf687c793f13ffb074842ddaf5e5b9bae5132
Manan Aggarwal <aggarwalmanan4@gmail.com>
2022-10-27 15:55:17 +0530

    added crashlytics

--- src/screens/shop/ShopScreen.js
+++ src/screens/shop/ShopScreen.js
@@ -1,37 +1,50 @@
 import { useState, useEffect } from 'react' ;
 import { Button } from 'react-native' ;
 import { RewardedAd, AdEventType, RewardedAdEventType, TestIds } from 'react-native-google-mobile-ads';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 import {MainView, KufamText} from '../../../cssApp.js' ;
 
+const adUnitId = TestIds.REWARDED ;
+// const adUnitId = 'ca-app-pub-7668722490423187/3649567094' ;
+
+const rewarded = RewardedAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
+
 const ShopScreen = ({navigation, route}) => {
-    // const adUnitId = TestIds.REWARDED ;
-    const adUnitId = 'ca-app-pub-7668722490423187/3649567094' ;
     
     const [loaded, setLoaded] = useState(false);
 
-    const rewarded = RewardedAd.createForAdRequest(adUnitId, {
-      requestNonPersonalizedAdsOnly: true,
-      keywords: ['fashion', 'clothing'],
-    });
-
     useEffect(() => {
-        // console.log('trying to load ad', rewarded) ;
+        // console.log('trying to load ad') ;
 
         const unsubscribeError = rewarded.addAdEventListener(AdEventType.ERROR, error => {
-            console.log('ad error ', error)
-            // setLoaded(true);
+            console.log('ad error ', error) ;
+            crashlytics().log('ad error ', error) ;
         });
 
         const unsubscribeLoaded = rewarded.addAdEventListener(RewardedAdEventType.LOADED, () => {
-            console.log('ad loaded ', adUnitId)
             setLoaded(true);
+            console.log('ad loaded ', adUnitId) ;
+            crashlytics().log('ad loaded ', adUnitId) ;
+        });
+
+        const unsubscribeClose = rewarded.addAdEventListener(AdEventType.CLOSED, error => {
+            console.log('ad closed') ;
+            crashlytics().log('ad closed') ;
+            setLoaded(false);
+       
+            //reload ad 
+            rewarded.load();
         });
 
         const unsubscribeEarned = rewarded.addAdEventListener(
             RewardedAdEventType.EARNED_REWARD,
             reward => {
                 console.log('User earned reward of ', reward);
+                crashlytics().log('User earned reward of ', reward) ;
             },
         );
 
@@ -39,8 +52,10 @@ const ShopScreen = ({navigation, route}) => {
 
         // Unsubscribe from events on unmount
         return () => {
-          unsubscribeLoaded();
-          unsubscribeEarned();
+          unsubscribeLoaded() ;
+          unsubscribeEarned() ;
+          unsubscribeClose() ;
+          unsubscribeError() ;
         };
     }, []);
 
